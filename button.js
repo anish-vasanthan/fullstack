aef2a010116b8595a5ebdfd2d99e1f28f993de1e
Anish <anishvasanthan510@gmail.com>
2025-08-24 20:01:28 +0530

    fullstack

---
+++ button.js
@@ -0,0 +1,6 @@
+let name = prompt("Enter your name:");
+let button = document.getElementById("colorBtn");
+
+button.addEventListener("click", function() {
+  document.body.style.backgroundColor = "lightgreen"; 
+});
